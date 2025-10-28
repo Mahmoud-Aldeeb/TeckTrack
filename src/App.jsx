d759e24a2968c923cb357a645f3ebcde7050267f
Mahmoud Aldeeb <mahmoudalden125@gmail.com>
2025-10-28 16:58:27 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-primary bg-secondary font-bold text-2xl'>
+        asdasdasdasd
+      </div>
+    </>
+  )
+}
+
+export default App
