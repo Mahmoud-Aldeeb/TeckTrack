00149920bb06c6a2dde1be445c2afde6c4d7e14f
Mahmoud Aldeeb <mahmoudalden125@gmail.com>
2025-11-18 21:19:58 +0200

    updata video on safari 1

--- src/pages/Roadmap/TrackDetails/VideoModal.jsx
+++ src/pages/Roadmap/TrackDetails/VideoModal.jsx
@@ -99,7 +99,7 @@ const VideoWithModal = ({ title, description, technologyId }) => {
                         <div className="relative pt-[56.25%]">
                             <iframe
                                 className="absolute inset-0 w-full h-full"
-                                src={`${embedUrl}?autoplay=1`}
+                                src={`${embedUrl}??autoplay=1&mute=1&playsinline=1&rel=0&modestbranding=1`}
                                 title={title}
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                                 allowFullScreen
